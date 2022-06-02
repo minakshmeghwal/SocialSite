62738225433533346fa1ad44b5414e30da710e87
minakshi <meenakshiparihar789@gmail.com>
2022-06-02 19:11:40 +0530

    ajax added

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -3,11 +3,24 @@ const comment=require('../models/comment')
 
 module.exports.create= async function(req,res)
 {   try{
-    await Post.create({
+    let post=await Post.create({
         content:req.body.content,
         user:req.user._id
     });
+    if(req.xhr)// check request is xml html request or not it is json request
+    {
+        return res.status(200).json({
+            data:{
+                post:post
+            },
+            message:"post created !"
+        });
+    }
+
+
+
     req.flash('success','Post published');
+
     return res.redirect('back');
 
     }catch(err)
